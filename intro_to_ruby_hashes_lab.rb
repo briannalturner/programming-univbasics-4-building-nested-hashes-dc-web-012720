b3ca4930e3190aa229b6a715513d43ced69089a2
Bri Turner <blturner3527@gmail.com>
2020-01-09 20:40:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,7 @@ def monopoly_with_third_tier
       :rent_in_dollars => new_hash = {
         one_piece_owned: 25,
         two_pieces_owned: 50,
-        one_piece_owned: 25,
+        three_pieces_owned: 100,
         ones_piece_owned: 25
       },
       :names => newer_hash = {
